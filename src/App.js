65be73cee0283e09391ddb83d895f751960f596e
charbelsako <charbelsarkis82581@gmail.com>
2018-08-28 15:30:23 +0300

    finally done with the merge and can continue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth'
 import './App.css'
 
 //Store
@@ -24,11 +25,31 @@ class App extends Component {
             <AppNavbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/:id" component={ClientDetails} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
-                <Route exact path="/login" component={Login} />
+                <Route
+                  exact
+                  path="/"
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route
+                  exact
+                  path="/client/add"
+                  component={UserIsAuthenticated(AddClient)}
+                />
+                <Route
+                  exact
+                  path="/client/:id"
+                  component={UserIsAuthenticated(ClientDetails)}
+                />
+                <Route
+                  exact
+                  path="/client/edit/:id"
+                  component={UserIsAuthenticated(EditClient)}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  component={UserIsNotAuthenticated(Login)}
+                />
               </Switch>
             </div>
           </div>
